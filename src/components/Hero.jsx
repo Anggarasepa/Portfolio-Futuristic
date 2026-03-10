7e4b9051cec1506386aaab303c391ea1e3631295
Asep Anggara <asepanggara@gmail.com>
2026-03-10 14:46:04 +0700

    tes20

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,7 +61,7 @@ const Hero = () => {
             transition={{ delay: 0.5 }}
             style={{ marginBottom: '30px' }}
           >
-            <span className="glitch-text" data-text="INNOVATOR">INNOVATOR</span>
+            <span className="glitch-text" data-text="System Architect">SYSTEM ARCHITECT</span>
           </motion.h1>
           
           {/* PERUBAHAN DI SINI: Text tanpa box */}
