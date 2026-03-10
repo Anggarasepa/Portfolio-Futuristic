38a6218a2024e598f8c4463e3780d844c5f54fbb
Asep Anggara <asepanggara@gmail.com>
2026-03-10 13:27:14 +0700

    salah jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About.jsx';
+import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import './styles/main.css';
